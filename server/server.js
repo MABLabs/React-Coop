dc2b230213a7cbd459e85a9394c0d1807c948a8d
Micheal <micheal.beaudoin@gmail.com>
2017-03-24 11:23:49 -0500

    Added web config files

---
+++ server/server.js
@@ -0,0 +1,70 @@
+var express = require('express'),
+    http = require('http'),
+    path = require('path'),
+    app = express(),
+    bodyParser = require('body-parser');
+
+//var getFiles = require('./query.js').getFiles,
+//    ept = require('./mbsdata.js').MbsData;
+
+var port = parseInt(process.argv[2] || '3000', 10);
+if (port < 1000)
+   console.log('Operating on Port '+port+' requires priveledge');
+
+app.set('port', port);
+
+
+// Is this even needed, especially in production ?
+//        to server up static explained files ???
+var home = path.join(__dirname, '..', 'public');
+app.use(express.static(home));
+
+// needed for when a form posts a JSON encoded body
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+var server = http.createServer(app);
+
+server.listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
+
+// -----------------------------------------------------------------------------
+//    TEST
+// -----------------------------------------------------------------------------
+
+app.get('/api/hello', function (req, res) {
+  var now = new Date();
+  res.send('Hello World on '+now);
+});
+
+// -----------------------------------------------------------------------------
+// Migrated from previous version of MbsNode
+// -----------------------------------------------------------------------------
+
+// http://ncsvmmbspreprod.nctr.fda.gov/query/files/05-01-2015/05-11-2015/*/M3
+// http://ncsvmmbspreprod.nctr.fda.gov/query/files/05-01-2015/05-11-2015/DIA/M3
+/*
+app.get('/api/query/files/:fromDate/:toDate/:otb/:subjects', function(req, res){
+
+    var files = getFiles('NCTR', req.params.fromDate, req.params.toDate, req.params.otb, req.params.subjects);
+    var names = JSON.stringify( files );
+    res.writeHead(200, {'Content-Type': 'application/json', 'Content-Length':names.length});
+    res.end(names);
+});
+
+app.post('/api/query/endpt/', function(req, res){
+
+    var ids = req.body.endpts;
+    if (typeof ids != 'Array')
+       ids = ids.split(/[ ,]/);
+
+    var getOneAnswer = function (results) {
+      var data = JSON.stringify( results );
+      res.writeHead(200, {'Content-Type': 'application/json', 'Content-Length':data.length});
+      res.end(data);
+    };
+
+    ept.getEndpt(req.body.filename, ids, getOneAnswer);
+});*/
+
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+module.exports = {
+  entry: './src/index.js',
+  output: {
+        path: './public/',
+        filename: 'bundle.js'
+    },
+  resolve:{
+        modulesDirectories: ['src', 'node_modules'],
+    },
+  devServer: {
+
+     // dont include boolean equivalent of these commandline switches, it will not work here
+     // these are in the package.json where the following is executed
+     //  webpack-dev-server --devtool eval-source --progress --colors --hot --inline --history-api-fallback
+
+     port: 8080,
+     contentBase: './public/',
+
+     // allow NodeJS to run side-by-side with webpack-dev-server
+     proxy: {  '/api/*': 'http://localhost:8081/' }   // <- backend
+  },
+  module: {
+    loaders: [
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel',
+            query: {
+                      presets: ['es2015', 'react']
+                   },
+          },
+
+          {
+              test: /\.css$/,
+              loaders: ['style', 'css']
+          },
+
+          // "file" loader for svg
+          {
+            test: /\.svg$/,
+            loader: 'file',
+            query: {
+              name: 'static/media/[name].[hash:8].[ext]'
+            }
+          },
+
+          {
+              test: /\.json$/,
+              loader: 'json'
+          }
+
+    ]
+  }
+}
