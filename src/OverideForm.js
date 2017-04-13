e5d9d47e12153f526b6eebba6612801028684500
Micheal <micheal.beaudoin@gmail.com>
2017-04-13 14:48:08 -0500

    Got gpio to work

--- src/OverideForm.js
+++ src/OverideForm.js
@@ -1,6 +1,25 @@
 import React, { Component } from 'react';
 import autoBind from 'react-autobind';
 import Switch from 'react-flexible-switch';
+import Toggle from 'react-toggle';
+import axios from 'axios';
+
+//import sleep from 'sleep';
+
+/*const lightsChange = (active) => {
+  var url = ""
+  if (active) {
+     console.log("Lights On");
+     url = `/api/light_on/`;
+  } else {
+     url = `/api/light_off/`;
+     console.log("Lights Off");
+  }
+
+  axios.get(url);
+  this.setState({ value: active })
+}
+*/
 
 class OverideForm extends Component {
     constructor(props) {
@@ -15,6 +34,7 @@ class OverideForm extends Component {
       this.state = {
         prop: 0
       };
+
 }
 
 checkValid() {
@@ -44,6 +64,72 @@ handleChange(event) {
       this.setState(stateChange);
 }
 
+lightsChange (active) {
+  var url = ""
+  if (active) {
+     console.log("Lights On");
+     url = `/api/lights_on/`;
+  } else {
+     url = `/api/lights_off/`;
+     console.log("Lights Off");
+  }
+
+  axios.get(url);
+  this.setState({ value: active })
+}
+
+doorChange (active) {
+  var url = ""
+  console.log("Active = ", active);
+  if (active) {
+     url = `/api/door_on/`;
+     axios.get(url);
+     console.log("Door On");
+//     sleep.sleep(30);
+     url = `/api/door_off/`;
+     axios.get(url);
+     console.log("door Off");
+  } else {
+     url = `/api/door_on/`;
+     axios.get(url);
+     console.log("Door On");
+//     sleep.sleep(30);
+     url = `/api/door_off/`;
+     axios.get(url);
+     console.log("door Off");
+  }
+
+  this.setState({ value: active })
+}
+
+heatChange (active) {
+  var url = ""
+  if (active) {
+     console.log("Heat On");
+     url = `/api/heat_on/`;
+  } else {
+     url = `/api/heat_off/`;
+     console.log("heat Off");
+  }
+
+  axios.get(url);
+  this.setState({ value: active })
+}
+
+fanChange (active) {
+  var url = ""
+  if (active) {
+     console.log("Fan On");
+     url = `/api/fan_on/`;
+  } else {
+     url = `/api/fan_off/`;
+     console.log("Fan Off");
+  }
+
+  axios.get(url);
+  this.setState({ value: active })
+}
+
 render() {
 
 var red = {color: 'rgb(255,0,0)', fontSize: '1.6em'};
@@ -52,15 +138,15 @@ var vs = (name) => (valid[name].length === 0) ? <span></span>: <span style={red}
 
 return <div>
          <h1>Coop Override</h1>
-         <div onChange={this.handleChange} >
+{/*         <div onChange={this.handleChange} > */}
            <div className="App-entry">
-             <b>Activate Light</b><Switch labels={{ on: 'On', off: 'Off' }} /><br />
-             <b>Activate Door</b><Switch labels={{ on: 'On', off: 'Off' }} /><br />
-             <b>Activate Fan</b><Switch labels={{ on: 'On', off: 'Off' }} /><br />
-             <b>Activate Heat</b><Switch labels={{ on: 'On', off: 'Off' }} /><br />
+             <b>Activate Light</b><Switch onChange={this.lightsChange} labels={{ on: 'On', off: 'Off' }} /><br />
+             <b>Activate Door</b><Switch onChange={this.doorChange} labels={{ on: 'On', off: 'Off' }} /><br />
+             <b>Activate Fan</b><Switch onChange={this.fanChange} labels={{ on: 'On', off: 'Off' }} /><br />
+             <b>Activate Heat</b><Switch onChange={this.heatChange} labels={{ on: 'On', off: 'Off' }} /><br />
            <hr />
            </div>
-         </div>
+{/*         </div> */}
        </div>
   }
 }
